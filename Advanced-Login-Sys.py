9fbca8a7bb5ec509e5c329150ff9786d96828254
amanyabubakr4484-eng <amanyabubakr4484@gmail.com>
2026-03-26 14:02:54 +0200

    Add files via upload

---
+++ Advanced-Login-Sys.py
@@ -0,0 +1,67 @@
+
+    
+print('Welcome to the Login-System')
+
+account=input('Do You have an account? yes or no\n').lower()
+
+correct_username='amany_m'
+correct_password='345m'
+
+if account=='yes':
+    username=input('please enter your username\n')
+    password=input('please enter your password\n')
+    
+    if username==correct_username and password==correct_password:
+        print('Welcome to the app')
+    else:
+        print('Wrong username or password\n Try again later')
+        
+else:
+    print('Please create an account')
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
